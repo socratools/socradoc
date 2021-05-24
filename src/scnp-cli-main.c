46114fb3fea6e93a98072c060e41e1f93736af2b
Hans Ulrich Niedermann <hun@n-dimensional.de>
2021-05-24 06:41:36 +0200

    Add the complete MIT license to the --version output

--- src/scnp-cli-main.c
+++ src/scnp-cli-main.c
@@ -296,7 +296,29 @@ void print_version(const char *const prog);
 static
 void print_version(const char *const prog)
 {
-    printf("%s (%s) %s\n", prog, PACKAGE_NAME, PACKAGE_VERSION);
+    printf("%s (%s) %s\n"
+           "Copyright (C) 2021 Jim Ramsay\n"
+           "Copyright (C) 2021 Hans Ulrich Niedermann\n"
+           "\n"
+           "Permission is hereby granted, free of charge, to any person obtaining a copy\n"
+           "of this software and associated documentation files (the \"Software\"), to deal\n"
+           "in the Software without restriction, including without limitation the rights\n"
+           "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n"
+"opies of the Software, and to permit persons to whom the Software is\n"
+           "furnished to do so, subject to the following conditions:\n"
+           "\n"
+           "The above copyright notice and this permission notice shall be included in all\n"
+           "copies or substantial portions of the Software.\n"
+           "\n"
+           "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n"
+           "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n"
+           "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n"
+           "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n"
+           "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n"
+           "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n"
+           "SOFTWARE.\n"
+           "",
+           prog, PACKAGE_NAME, PACKAGE_VERSION);
 }
 
 
