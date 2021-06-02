37ac86e3194e4576db6a55b0d7d7c4211cb2a5c0
Hans Ulrich Niedermann <hun@n-dimensional.de>
2021-06-02 08:49:22 +0200

    test: Set SCNP_CLI_DRY_RUN=yes to allow for more tests
    As the test suite now uses SCNP_CLI_DRY_RUN, we do not need to test
    for a device any more: We will just dry-run the command.
    
    This means we do not need WC or LSUSB any more.
    
    Now we can finally check most of the \fBscnp\-cli\fR command
    parameters from test scripts.

--- test/scnp-cli_audio-routing_3.sh
+++ test/scnp-cli_audio-routing_3.sh
@@ -1,14 +1,3 @@
 #!/bin/sh
 
-device_count="$(${LSUSB-lsusb} -d 05fc:0030 -d 05fc:0031 -d 05fc:0032 | ${WC-wc} -l)"
-if test "x$device_count" = "x0"
-then
-    echo "No device found."
-    exit 77
-elif test "x$device_count" = "x1"
-then
-    ${SCNP_CLI-scnp-cli} audio-routing 3
-else
-    echo "Too many devices found."
-    exit 77
-fi
+${SCNP_CLI-scnp-cli} audio-routing 3
