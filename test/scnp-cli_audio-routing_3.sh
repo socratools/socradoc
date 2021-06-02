3860a14a30ad1d62088002990af899aa2b9dcaf1
Hans Ulrich Niedermann <hun@n-dimensional.de>
2021-06-02 08:48:51 +0200

    test: Use env setup instead of generating separate script files
    Set up environment variables in the test suite and then
    use the variables from inside the test scripts.
    
    This is less complex than generating test scripts
    *.sh from *.shin while substituting values.

--- test/scnp-cli_audio-routing_3.sh
+++ test/scnp-cli_audio-routing_3.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-device_count="$(@LSUSB@ -d 05fc:0030 -d 05fc:0031 -d 05fc:0032 | @WC@ -l)"
+device_count="$(${LSUSB-lsusb} -d 05fc:0030 -d 05fc:0031 -d 05fc:0032 | ${WC-wc} -l)"
 if test "x$device_count" = "x0"
 then
     echo "No device found."
     exit 77
 elif test "x$device_count" = "x1"
 then
-    @abs_top_builddir@/scnp-cli audio-routing 3
+    ${SCNP_CLI-scnp-cli} audio-routing 3
 else
     echo "Too many devices found."
     exit 77
